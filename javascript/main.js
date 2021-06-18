ab861b302ee594bf6972e00483317725a83bae1b
eslam1998-dev <eslam1998-dev>
2021-06-18 11:10:05 +0200

    my first modified file

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,82 @@
 //set max-width to parent-sections
 $(function () {
     function mainWidth() {
-        let remainWidth = $(window).innerWidth() - $(".parent .nav-bar").outerWidth();
-        $(".parent .parnet-sections").css("width",remainWidth)
-        console.log(remainWidth)
+        const ele = $(".parent .nav-bar");
+        let winWidth = $(window).innerWidth();
+        let remainWidth = winWidth - $(".parent .nav-bar").outerWidth();
+        console.log($(".parent .nav-bar").outerWidth())
+        if (winWidth <= 1200) {
+            ele.addClass("active")
+            $(".parent .parnet-sections").css("width",winWidth)
+        } else {
+            $(".parent .parnet-sections").css("width",remainWidth);
+            ele.removeClass("active")
+        }
     }
     mainWidth()
     $(window).resize(function () {
         mainWidth()
     })
+})
+
+//coloring web
+//translate setting section
+$(".parnet-sections .setting .icon").on("click", function () {
+    const eleWidth = $(this).parent().outerWidth();
+    if ($(this).hasClass("active")) {
+        $(this).parent().animate({
+            right: - eleWidth + "px"
+        }).children(".icon").removeClass("active")
+    } else {
+        $(this).parent().animate({
+            right:0
+        }).children(".icon").addClass("active")
+    }
+})
+
+const targetElemet = $(".parent .nav-bar .row ul");
+$(".parnet-sections .setting .colors li").on("click", function () {
+    $(".bgcolor-style." + $(this).attr("class")).css("background-color","var(--" + $(this).attr("class") + ")")
+    $(".bgcolor-style.fas").css({
+        "background-color":"white",
+        "color":"var(--" + $(this).attr("class") + ")"
+    })
+    
+    targetElemet.attr("class",$(this).attr("class")) //add class to parent of list [ul]
+    
+    $(".parent .nav-bar .row ul li.active")
+    .attr("class","heading " + targetElemet.attr("class") + " active")
+
+})
+
+$(".parent .nav-bar .row ul li").on("click", function () {
+    $(this).addClass("heading " + targetElemet.attr("class") + " active")
+    .siblings().removeClass("" + targetElemet.attr("class") + " active")
+})
+
+//add classess
+$(".bgcolor-style,.parent .nav-bar .fa-bars").each(function () {
+    const colorArr = ["redColor","blueColor","OrangeColor","YellowColor","greenColor"];
+    colorArr.map(e => {
+        $(this).addClass(e)
+    })
+})
+
+//switch between dark/light mode
+$(".parnet-sections .input input").on("click", function () {
+    $(".parent").attr("class","parent " + $(this).attr("class"))
+})
+
+//translate nav-bar section
+$(".bgcolor-style.fas.fa-bars").on("click", function () {
+    $(this).toggleClass("fa-times active");
+    if ($(this).hasClass("active")) {
+        $(".parent .nav-bar").animate({
+            "left":0
+        })
+    } else {
+        $(".parent .nav-bar").animate({
+            "left":-$(".parent .nav-bar").outerWidth()
+        })
+    }
 })
