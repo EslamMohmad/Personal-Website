46a1d0e4bf8b8b322b7b80d290c2c107912fc6a5
eslam1998-dev <eslam1998-dev>
2021-06-21 16:54:26 +0200

    final modified file

--- javascript/main.js
+++ javascript/main.js
@@ -7,9 +7,11 @@ $(function () {
         if (winWidth <= 1200) {
             ele.addClass("active")
             $(".parent .parnet-sections").css("width",winWidth)
+            $(".parent .section-four .container .over-lay").css("width",winWidth)
         } else {
             $(".parent .parnet-sections").css("width",remainWidth);
-            ele.removeClass("active")
+            ele.removeClass("active");
+            $(".parent .section-four .container .over-lay").css("width",remainWidth)
         }
     }
     mainWidth()
@@ -18,7 +20,6 @@ $(function () {
     })
 })
 
-//coloring web
 //translate setting section
 $(".parnet-sections .setting .icon").on("click", function () {
     const eleWidth = $(this).parent().outerWidth();
@@ -32,27 +33,26 @@ $(".parnet-sections .setting .icon").on("click", function () {
         }).children(".icon").addClass("active")
     }
 })
-
+//coloring web
 const targetElemet = $(".parent .nav-bar .row ul");
 $(".parnet-sections .setting .colors li").on("click", function () {
-    $(".bgcolor-style." + $(this).attr("class")).css("background-color","var(--" + $(this).attr("class") + ")")
-    $(".bgcolor-style.fas").css({
-        "background-color":"white",
-        "color":"var(--" + $(this).attr("class") + ")"
-    })
+
+    //add bgcolor to all elements that have bgcolor-style class
+    $(".bgcolor-style").css("background-color","var(--" + $(this).attr("class") + ")")
     
-    targetElemet.attr("class",$(this).attr("class")) //add class to parent of list [ul]
+    //add class to parent of list [ul]
+    targetElemet.attr("class",$(this).attr("class"))
     $(".parent .nav-bar .row ul li.active")
     .attr("class","heading " + targetElemet.attr("class") + " active")
 
-    //add color to any text
+    //add color to any text that has color-style class
     $(".color-style").css("color","var(--" + $(this).attr("class") + ")")
 
-    let THIcon = $(".parent .section-three .bord").attr("class","bord " + $(this).attr("class"))
+    //add color-class to services section
     $(".parent .section-three .row .content").attr("class","content grid " + $(this).attr("class"))
 
-    //sections-four ul li
-    $(".parent .section-four .content .filter-list ul li").css("border-color","var(--"+ $(this).attr("class") + ")")
+    //add border-color-class to portofolio section
+    $(".parent .section-four .content .filter-list ul").attr("class",$(this).attr("class"))
 })
 
 //when click on nav-bar items
@@ -61,21 +61,13 @@ $(".parent .nav-bar .row ul li").on("click", function () {
     .siblings().removeClass("" + targetElemet.attr("class") + " active")
 })
 
-//add classess
-$(".bgcolor-style,.parent .nav-bar .fa-bars").each(function () {
-    const colorArr = ["redColor","blueColor","OrangeColor","YellowColor","greenColor"];
-    colorArr.map(e => {
-        $(this).addClass(e)
-    })
-})
-
 //switch between dark/light mode
 $(".parnet-sections .input input").on("click", function () {
     $(".parent").attr("class","parent " + $(this).attr("class"))
 })
 
 //translate nav-bar section
-$(".bgcolor-style.fas.fa-bars").on("click", function () {
+$(".color-style.fas.fa-bars").on("click", function () {
     $(this).toggleClass("fa-times active");
     if ($(this).hasClass("active")) {
         $(".parent .nav-bar").addClass("translated")
@@ -100,7 +92,7 @@ $(".parent .section-four .content .filter-list ul li").on("click", function () {
     let item = $(".bord." + $(this).text());
     const parent = $(".parent .section-four .content .grid").children()
     if ($(this).text() == "all") {
-        parent.fadeIn().parent().removeClass("short")
+        parent.fadeIn().parent().removeClass("short") // grid setting
     } else {
         parent.fadeOut(function () { parent.parent().addClass("short") })
         item.delay(400).fadeIn()
